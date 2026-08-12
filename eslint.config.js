7ba6f14e827859ac1879333727bd12c249b01130
Marty Yavorcik <tesla6699@protonmail.com>
2026-08-12 07:34:35 -0400

    Position Atlas Nuclear as SMR readiness platform

--- eslint.config.js
+++ eslint.config.js
@@ -18,4 +18,10 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+  {
+    files: ['netlify/functions/*.js', 'scripts/*.mjs'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
 ])
